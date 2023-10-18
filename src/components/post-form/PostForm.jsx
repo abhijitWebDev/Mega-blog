fa1e068f865c2c1809e10239c854a29a6ef9c253
abhijitWebDev <110275876+abhijitWebDev@users.noreply.github.com>
2023-10-18 17:17:29 +0530

    removed console.log

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -19,7 +19,6 @@ export default function PostForm({ post }) {
     const userData = useSelector((state) => state.auth.userData);
 
     const submit = async (data) => {
-        console.log(userData)
         if (post) {
             const file = data.image[0] ? await appwriteService.uploadFile(data.image[0]) : null;
 
